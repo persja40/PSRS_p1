8ef6f4860480457df4935ebb340e5c25f0a68ba9
persja40 <persja40@gmail.com>
2018-03-21 12:07:34 +0100

    a

--- main.cpp
+++ main.cpp
@@ -8,35 +8,38 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+	//PHASE I
 	vector<int> arr{};
-	//read data from file
-	ifstream ifile;
-	ifile.open("example.txt");
-	while (!ifile.eof())
-	{
-		int t;
-		ifile >> t;
-		arr.push_back(t);
-	}
-	ifile.close();
-
-	//test
-	sort(begin(arr), end(arr));
-
 	int myid, numprocs;
 	MPI::Init(argc, argv);
 	numprocs = MPI::COMM_WORLD.Get_size();
 	myid = MPI::COMM_WORLD.Get_rank();
 
+	if (myid == 0)
+	{ //read data from file
+		ifstream ifile;
+		ifile.open("example.txt");
+		while (!ifile.eof())
+		{
+			int t;
+			ifile >> t;
+			arr.push_back(t);
+		}
+		ifile.close();
+	}
+
+	//PHASE II
+
+	//test
+	//sort(begin(arr), end(arr));
+
+	//ENDING
 	//write to file
 	ofstream ofile;
 	ofile.open("result.txt");
 	for (auto &e : arr)
 		ofile << e << " ";
 	ofile.close();
-
-	cout << endl
-		 << "Hello world" << endl;
 
 	MPI::Finalize();
 	return 0;
